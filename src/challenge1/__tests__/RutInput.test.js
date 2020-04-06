fc3a0b4db8146050e2f5bb9574cc079148e4cce8
Rodrigo Martinez Diaz <martinez.d.rodrigo@gmail.com>
2020-04-06 04:05:18 -0400

    updated acceptance criteria of challenge 1

--- src/challenge1/__tests__/RutInput.test.js
+++ src/challenge1/__tests__/RutInput.test.js
@@ -9,14 +9,15 @@ import RutInput from "../RutInput";
  *
  * Criterios de Aceptacion
  *
- * 1. El input solo debe aceptar digitos
- * 2. El maximo numero de digitos es de 10
+ * 1. El input solo debe aceptar digitos y el caracter K o k
+ * 2. El maximo numero de digitos es de 9
  * 3. Al ingresar digitos estos se van formateando usando puntos cada 3 digitos de derecha a izquierda
  *      Ej si ingreso 123  -> 123
  *         si ingreso 1234 -> 1.234
  *         si ingreso 12345123 -> 12.345.123
  * 4. Al ingresar sobre 7 digitos el ultimo digito es separado del resto usando un guion
  *      Ej si ingreso 12345678 -> 1.234.567-8
+ * 5. El input debe ser formateado a minusculas
  */
 
 describe("RutInput component", () => {
@@ -25,4 +26,6 @@ describe("RutInput component", () => {
     xit("should format render value adding a point as preffix given three consecutives digits", () => {});
 
     xit("should separate last character using an hyphen for strings with length greater than 7 characters", () => {});
+
+    xit("should format input value to lowercase", () => {});
 });
